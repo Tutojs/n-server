af6c8455ca4fdc71004b842f20d721764c927865
Tuto <51621022+Tutojs@users.noreply.github.com>
2019-06-10 16:19:58 +0430

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 'use strict'
-// node --experimental-modules server.js
+// to use ECMAscript modules: node --experimental-modules server.js
+
+// modules START
 import http from 'http'
 import http2 from 'http2'
 import url from 'url'
 import fs from 'fs'
 import path from 'path'
+// modules END
 
-const hostname = '[::1]' // localhost: IPV6
-const port = {http: 80, https: 443}
+// host START
 
+const hostname = '[::1]' // localhost in IPV6
+const port = {http: 80, https: 443} // default port of http and https
+// host END
+
+// __dirname and __filename don't exist in ECMAscript modules
+// import.meta is equivalence witch return url of module {url: "url-of-module"}
 const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-//http server START
-// redirect all http to https
+// http server START
+// redirect all http request to https
 // http 301: Moved Permanently
 http.createServer((request, response) => {
   response
@@ -23,8 +31,10 @@ http.createServer((request, response) => {
   .listen(port.http, hostname)
 //http server END
 
-//http2 secure server START
-//create key and cert files by openssl, self-signed ssl certificate
+// http2 secure server START
+// to create key and cert files for self-signed ssl certificate use openssl, or use https://www.ssl.com/online-csr-and-key-generator
+// set "allowHTTP1: true" to support both http 1 and 2 clients
 http2.createSecureServer({key: fs.readFileSync('private.key'), cert: fs.readFileSync('certificate.cert'), allowHTTP1: true}, (request, response) => {
   console.log(JSON.stringify({method: request.method, url: request.url})))
-//http2 secure server END
+})
+// http2 secure server END
