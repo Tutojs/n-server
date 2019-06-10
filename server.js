e13cda30abb036e2b4d00612de528a091bb16a84
Tuto <51621022+Tutojs@users.noreply.github.com>
2019-06-10 17:51:20 +0430

    Update server.js

--- server.js
+++ server.js
@@ -36,5 +36,69 @@ http.createServer((request, response) => {
 // set "allowHTTP1: true" to support both http 1 and 2 clients
 http2.createSecureServer({key: fs.readFileSync('private.key'), cert: fs.readFileSync('certificate.cert'), allowHTTP1: true}, (request, response) => {
   console.log(JSON.stringify({method: request.method, url: request.url})))
+  if (request.method === 'POST' && request.headers['content-type'] === 'application/x-www-form-urlencoded') {
+    let body = Buffer.alloc(0)
+    request
+      .on('error', error => console.error(error))
+      .on('data', chunk => {
+        body = Buffer.concat([body, chunk])
+        if (Buffer.byteLength(body) > 1024**2) {
+          request
+            .connection.destroy()
+        }
+      })
+      .on('end', () => {
+        // check if "end" event execution due to request.connection.destroy()
+        if (request.connection.destroyed === true) {
+          response
+            .writeHead(413, {"Content-type": "text/html"})
+            .end(`<h1>entry data is too large</h1>`)
+        }
+        else {
+          body = url.parse(`?${body.toString()}`, true).query
+          // body = {key-1: value-1, key-2: value-2, ..., key-n, value-n}
+          // do anything with data
+          response
+            .writeHead(200, {"Content-type": "text/html"})
+            .end(`your info:\n<h2>${JSON.stringify(body)}</h2>`)
+        }
+        return
+      })
+  }
+  else if (request.method === 'POST' && request.headers['content-type'].match(/^.*?(?=; )/g)[0] === 'multipart/form-data') {
+    let raw_boundary = request.headers['content-type'].match(/(?<=boundary=).*?$/g)[0]
+    let first_boundary = Buffer.from(`--${raw_boundary}\r\n`)
+    // let middle_boundary = Buffer.from(`\r\n${raw_boundary}--\r\n`)
+    let last_boundary = Buffer.from(`\r\n--${raw_boundary}--`)
+    let multipart = Buffer.alloc(0)
+    request
+      .on('error', error => {
+        response
+          .writeHead(422, {"Content-type": "text/html"})
+          .end(`<h1>${error.message}</h1>`)
+      })
+      .on('data', chunk => {
+        multipart = Buffer.concat([multipart, chunk])
+        if (Buffer.byteLength(multipart) > this.options.maximum_field_inputs_size + this.options.maximum_file_inputs_size) {
+          request
+            .connection.destroy() // immediately fire 'end' event -> request.connection.destroyed will set to 'true'
+        }
+      })
+      .on('end', ()=> {
+        if (request.connection.destroyed === true) {
+          response
+            .writeHead(413, {"Content-type": "text/html"})
+            .end(`<h1>entry data is too large</h1>`)
+        }
+        else {
+          let header_start = multipart.indexOf(first_boundary) + first_boundary.length
+          let header_end = multipart.indexOf(Buffer.from('\r\n\r\n'))
+          let header = multipart.slice(header_start, header_end).toString()
+          
+          let start_data = header_end + header_end.length
+          let end_data = multipart.indexOf(last_boundary)
+          let data = multipart.slice(header_end + header_end.length, end_data)
+      }
+  }
 })
 // http2 secure server END
