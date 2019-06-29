afa05da00ad7a00ac8151dce9c9c72e3a8ef3810
Tuto <51621022+Tutojs@users.noreply.github.com>
2019-06-29 23:36:17 +0430

    Update server.js

--- server.js
+++ server.js
@@ -354,7 +354,7 @@ http2.createSecureServer({key: fs.readFileSync('private.key'), cert: fs.readFile
                         <span class="text.secondary">permalink <a href="https://${domain}${name}">#</a></span>
                         <h5>${new Date().getFullYear()}-${new Date().getMonth() + 1}-${(Math.random()*30).toFixed(0)}</h5>
                       </div>
-                      <div class="card-body"
+                      <div class="card-body">
                       ${content}
                       </div>
                       <div class="card-footer">
@@ -376,7 +376,7 @@ http2.createSecureServer({key: fs.readFileSync('private.key'), cert: fs.readFile
                       <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email">
                       <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
-                    <div class="form-group"><!--node --experimental-modules server.js-->
+                    <div class="form-group">
                       <label for="exampleInputPassword1">Password</label>
                       <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
                     </div>
